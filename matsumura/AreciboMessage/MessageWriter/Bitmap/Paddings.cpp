ca4f6c62b30c918149322d01d99604c16ebe717b
taishi-matsumura <taishi0706@gmail.com>
2020-01-22 22:02:44 +0900

    :sparkles: :white_check_mark: パディングクラスを作成
    - ビットマップデータのパディングバイトを管理するクラスを作成
    - テストコードを追加

---
+++ matsumura/AreciboMessage/MessageWriter/Bitmap/Paddings.cpp
@@ -0,0 +1,33 @@
+﻿#include "Paddings.h"
+
+namespace arecibo_message {
+namespace message_writer {
+namespace bitmap {
+
+// ========================================================================================
+// Constructor
+// ========================================================================================
+Paddings::Paddings(Size sizeWithoutPaddings) {
+	// まず端数を求める
+	auto remainder = sizeWithoutPaddings % 4;
+
+	if (remainder != 0) {
+		// 4の倍数になるよう、0でパディングする
+		// パディングするサイズは4から端数を引いた数
+		auto size = 4 - remainder;
+		paddings.resize(size, BYTE(0));
+	}
+}
+
+// ========================================================================================
+// Public Methods
+// ========================================================================================
+void Paddings::Write(std::ostream& stream) const {
+	for (auto&& padding : paddings) {
+		stream << padding;
+	}
+}
+
+} // namespace bitmap
+} // namespace message_writer
+} // namespace arecibo_message
---
+++ matsumura/AreciboMessageTest/MessageWriter/Bitmap/PaddingsTest.cpp
@@ -0,0 +1,81 @@
+﻿#include "pch.h"
+
+#include <random>
+#include <MessageWriter/Bitmap/Paddings.h>
+
+namespace arecibo_message {
+namespace message_writer {
+namespace bitmap {
+
+class PaddingsTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        mt.seed(rnd());
+    }
+
+    void SetSize() {
+        while (true) {
+            sizeWithoutPaddings = mt();
+
+            if (sizeWithoutPaddings % 4 == remainder) return;
+        }
+    }
+
+    std::mt19937 mt;
+    std::random_device rnd;
+
+    Size remainder;
+    Size sizeWithoutPaddings;
+};
+ 
+TEST_F(PaddingsTest, SizeEq0) {
+    // Arrange
+    remainder = 0;
+    SetSize();
+
+    // Act
+    Paddings target(sizeWithoutPaddings);
+
+    // Assert
+    EXPECT_EQ(0, target.GetSize());
+}
+
+TEST_F(PaddingsTest, SizeEq1) {
+    // Arrange
+    remainder = 3;
+    SetSize();
+
+    // Act
+    Paddings target(sizeWithoutPaddings);
+
+    // Assert
+    EXPECT_EQ(1, target.GetSize());
+}
+
+TEST_F(PaddingsTest, SizeEq2) {
+    // Arrange
+    remainder = 2;
+    SetSize();
+
+    // Act
+    Paddings target(sizeWithoutPaddings);
+
+    // Assert
+    EXPECT_EQ(2, target.GetSize());
+}
+
+TEST_F(PaddingsTest, SizeEq3) {
+    // Arrange
+    remainder = 1;
+    SetSize();
+
+    // Act
+    Paddings target(sizeWithoutPaddings);
+
+    // Assert
+    EXPECT_EQ(3, target.GetSize());
+}
+
+} // namespace bitmap
+} // namespace message_writer
+} // namespace arecibo_message
