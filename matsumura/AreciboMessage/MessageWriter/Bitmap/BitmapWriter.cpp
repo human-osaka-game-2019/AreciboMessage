a1cf6c266fd23e92a733ae2b54a64e4377d13123
taishi-matsumura <taishi0706@gmail.com>
2020-01-22 22:12:38 +0900

    :sparkles: アレシボメッセージをビットマップファイルに出力するクラスを作成

---
+++ matsumura/AreciboMessage/MessageWriter/Bitmap/BitmapWriter.cpp
@@ -0,0 +1,50 @@
+﻿#include "BitmapWriter.h"
+
+#include <fstream>
+
+#include <shellapi.h>
+#include <Windows.h>
+#include "MessageWriter/Constants.h"
+#include "PixelIndices.h"
+#include "Utility/Settings.h"
+
+namespace arecibo_message {
+namespace message_writer {
+namespace bitmap {
+
+// ========================================================================================
+// Constructor
+// ========================================================================================
+BitmapWriter::BitmapWriter(const message_data::BitContainer& bytes)
+	: MessageWriterBase<Color>(bytes, [](bool bit) { return bit ? Color::White() : Color::Black(); }) {
+	// 設定情報から拡大率を取得する
+	auto& settings = utility::Settings::Instance();
+	scale = settings.GetIntValue(TEXT("Bitmap"), TEXT("Scale"));
+
+	pBitmap = std::make_unique<Bitmap>(MESSAGE_WIDTH * scale, MESSAGE_HEIGHT * scale);
+}
+
+// ========================================================================================
+// Overridden Protected Methods
+// ========================================================================================
+void BitmapWriter::WriteCell(Size row, Size col, OutputType value) {
+	// メッセージの行と列に対応するビットマップのピクセル番号を算出する
+	PixelIndices indices(row, col, scale);
+
+	// 算出した各ピクセルに色を設定
+	indices.ApplyToAll([=](Size& index) { (*pBitmap)[index] = value; });
+}
+
+void BitmapWriter::Finalize() {
+	// ファイルに書き出す
+	std::ofstream stream("AreciboMessage.bmp", std::ios::binary);
+	pBitmap->Write(stream);
+	stream.close();
+
+	// 書き出したビットマップファイルを既定のアプリで開く
+	ShellExecute(NULL, "open", TEXT("AreciboMessage.bmp"), NULL, NULL, SW_SHOWNORMAL);
+}
+
+} // namespace bitmap
+} // namespace message_writer
+} // namespace arecibo_message
