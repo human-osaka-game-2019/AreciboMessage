43b2582c4fdbe5330aaf301086ea077b2cad0a48
taishi-matsumura <taishi0706@gmail.com>
2020-01-22 22:15:03 +0900

    :sparkles: アレシボメッセージクラスを作成
    アレシボメッセージクラスをアプリ層から使用し、メッセージを出力する処理を作成

---
+++ matsumura/AreciboMessage/AreciboMessage.h
@@ -0,0 +1,31 @@
+﻿#ifndef ARECIBO_MESSAGE_H_
+#define ARECIBO_MESSAGE_H_
+
+#include "IUncopyable.h"
+#include "TypeAliases.h"
+
+namespace arecibo_message {
+
+/// <summary>
+/// アレシボメッセージクラス
+/// </summary>
+class AreciboMessage : private IUncopyable {
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="message">32ビットのデータ列(ビッグエンディアン)</param>
+	explicit AreciboMessage(const MessageSource& message);
+
+	~AreciboMessage();
+
+	/// <summary>
+	/// アレシボメッセージを出力する
+	/// </summary>
+	/// <remarks>出力先等はconfig.iniに記述すること</remarks>
+	void Output() const;
+};
+
+} // namespace arecibo_message
+
+#endif // !ARECIBO_MESSAGE_H_
