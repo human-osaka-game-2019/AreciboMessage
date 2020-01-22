25fb80d6faab6f713bebe24fd8584f51ba9791ea
taishi-matsumura <taishi0706@gmail.com>
2020-01-22 22:13:20 +0900

    :sparkles: アレシボメッセージ出力クラスのファクトリーを作成

---
+++ matsumura/AreciboMessage/MessageWriter/MessageWriterFactory.h
@@ -0,0 +1,35 @@
+﻿#ifndef MESSAGE_WRITER_FACTORY_H_
+#define MESSAGE_WRITER_FACTORY_H_
+
+#include <memory>
+
+#include "MessageWriter/IMessageWriter.h"
+#include "Utility/Singleton.h"
+
+namespace arecibo_message {
+namespace message_writer {
+
+/// <summary>
+/// MessageWriterのファクトリークラス
+/// </summary>
+class MessageWriterFactory : public utility::Singleton<MessageWriterFactory> {
+public:
+	/// <summary>
+	/// MessageWriterを生成する
+	/// </summary>
+	/// <param name="message">32ビットのデータの集合</param>
+	/// <returns>MessageWriterのインスタンス</returns>
+	IMessageWriter& CreateWriter(const MessageBits& message);
+
+private:
+	friend utility::Singleton<MessageWriterFactory>;
+
+	MessageWriterFactory() {}
+
+	std::unique_ptr<IMessageWriter> pWriter;
+};
+
+} // namespace message_writer
+} // namespace arecibo_message
+
+#endif // !MESSAGE_WRITER_FACTORY_H_
